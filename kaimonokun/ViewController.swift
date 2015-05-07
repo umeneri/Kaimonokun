679fd4aa58c3eb99818df49e1a1afe50844e00e1
umeneri <mokushiroku97@gmail.com>
2015-05-07 10:04:29 +0900

    Initial Commit

---
+++ kaimonokun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kaimonokun
+//
+//  Created by umeneri on 2015/05/07.
+//  Copyright (c) 2015年 umeneri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
